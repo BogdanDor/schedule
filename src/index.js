523c008ac54c4c11f3b6c896d7318dcbc72c4634
Bogdan Dor <bogdandoroschenko@gmail.com>
2019-11-26 21:47:23 +0300

    Create classes that store all necessary information about components

--- src/index.js
+++ src/index.js
@@ -1,43 +1,64 @@
+function render(element, container) {
+  const rootComponent = instantiateComponent(element);
+  container.appendChild(rootComponent.mount());
+}
+
+function instantiateComponent(element) {
+  const type = element.type;
+  if (typeof type === 'function') {
+    return new CompositeComponent(element);
+  } else if (isHostElement(type)) {
+    return new HostComponent(element);
+  }
+}
+
 function isHostElement(typeOfElement) {
   const types = ['a', 'button', 'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'img', 'li', 'ol', 'p', 'span', 'svg', 'ul'];
   return types.includes(typeOfElement);
 }
 
-function mountComposite(element) {
-  const type = element.type;
-  const props = element.props;
-  const publicInstanse = type(props);
-  return mount(publicInstanse);
-}
+class CompositeComponent {
+  constructor(element) {
+    this.currentElement = element;
+    this.renderedComponent = null;
+  }
 
-function mountHost(element) {
-  const type = element.type;
-  const props = element.props;
-  const parentNode = document.createElement(type);
-  Object.keys(props).forEach(propName => {
-    if (propName !== 'children') {
-      node.setAttribute(propName, props[propName]);
-    }
-  });
-  props.children.forEach(child => {
-    const childNode = mount(child);
-    parentNode.appendChild(childNode);
-  });
-  return parentNode;
-}
-
-function mount(element) {
-  const type = element.type;
-  if (typeof type === 'function') {
-    return mountComposite(element);
-  } else if (isHostElement(type)) {
-    return mountHost(element);
+  mount() {
+    const type = this.currentElement.type;
+    const props = this.currentElement.props;
+    let renderedComponent = this.renderedComponent;
+    let renderedElement;
+    renderedElement = type(props);
+    renderedComponent = instantiateComponent(renderedElement);
+    this.renderedComponent = renderedComponent;
+    return renderedComponent.mount();
   }
 }
 
-function render(element, container) {
-  const node = mount(element);
-  container.appendChild(node);
+class HostComponent {
+  constructor(element) {
+    this.currentElement = element;
+    this.node = null;
+  }
+
+  mount() {
+    const type = this.currentElement.type;
+    const props = this.currentElement.props;
+    let node = this.node;
+    node = document.createElement(type);
+    Object.keys(props).forEach(propName => {
+      if (propName !== 'children') {
+        node.setAttribute(propName, props[propName]);
+      }
+    });
+    props.children.forEach(child => {
+      const childComponent = instantiateComponent(child);
+      const childNode = childComponent.mount();
+      node.appendChild(childNode);
+    });
+    this.node = node;
+    return node;
+  }
 }
 
 export default render;
